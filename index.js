f37a5f37218fe2c9c9d64f6acbd3196b71d63a2a
George Cope <G.Cope13@gmail.com>
2021-02-16 11:46:50 +0000

    Included packages necessary for application, in addition to the generateMarkdown file in the utils folder

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // "What is the title of your project?",
+    // "Write a description of the project: ",
+    // "Provide installation instructions for your project: ",
+    // "How do you use the application associated with your project?",
+    // "Which license does the project fall under?",
+    // "Please enter any contribution guidelines you may have: ",
+    // "Please describe any testing you have carried out on your project: ",
+    // "Please enter your contact information should anyone have any questions: ",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
