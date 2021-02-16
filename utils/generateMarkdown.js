01c37daafdaa9db89b56bffc5240aef5d8284319
George Cope <G.Cope13@gmail.com>
2021-02-16 22:10:34 +0000

    Developed renderLicenceBadge function using a switch statement to select which license badge is show in the README depending on which option the user inputs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,15 +2,50 @@
 // If there is no license, return an empty string
 
 function renderLicenseBadge(data) {
-  let badgeLink;
-  if(data.license !== ""){
-    badgeLink=`[![GitHub license](https://img.shields.io/github/license/MyDryDay/${data.title}.svg)](https://github.com/MyDryDay/${data.title}/blob/main/LICENSE)`;
-    return badgeLink;
-  }else if(data.license == "No License"){
-    badgeLink="";
-    return badgeLink;
+// [![GitHub license](https://img.shields.io/github/license/MyDryDay/${data.title}.svg)](https://github.com/MyDryDay/${data.title}/blob/main/LICENSE)`; 
+
+  constlicenseLink=``;
+  const gitLicense=data.license;
+
+  switch(gitLicense){
+    case `Apache 2.0`:
+      licenseLink=`[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case `GNU General Public 3.0`:
+      licenseLink=`[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case `MIT`:
+      licenseLink=`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+    case `BSD 2-Clause`:
+      licenseLink=`[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+      break;
+    case `BSD 3-Clause`:
+      licenseLink=`[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+      break;
+    case `Creative Commons Zero v1.0`:
+      licenseLink=`[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+      break;
+    case `Eclipse Public 2.0`:
+      licenseLink=`[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      break;
+    case `GNU Affero General Public v3.0`:
+      licenseLink=`[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+      break;
+    case `GNU General Public v2.0`:
+      licenseLink=`[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      break;
+    case `Mozilla Public 2.0`:
+      licenseLink=`[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+      break;
+    case `The Unlicense`:
+      licenseLink=`[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+      break;
+    case `No License`:
+      licenseLink=``;
+      break;
   }
-  // [![GitHub license](https://img.shields.io/github/license/**USERNAME**/**REPO_NAME**/.svg)](https://github.com/**USERNAME**/**REPO_NAME**/blob/main/LICENSE)
+
 }
 
 // TODO: Create a function that returns the license link
@@ -23,6 +58,10 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  renderLicenseBadge(data);
+
+
   const markDown = `# ${data.title} \
   # Description \
   ${data.description} \
@@ -30,7 +69,7 @@ function generateMarkdown(data) {
   ${data.install} \
   ### Usage \
   ${data.instructions} \
-  #### License ${renderLicenseBadge(data)}\
+  #### License ${licenseLink} \
   ${data.license} \
   renderLicenseLink(); \
   renderLicenseSection(); \
