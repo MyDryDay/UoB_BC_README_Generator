9139faf56a2eb375c3d4225f5b92d16c05cb8f4e
George Cope <G.Cope13@gmail.com>
2021-02-18 07:05:51 +0000

    Developed renderLicenseSection function using if/else statement

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -91,7 +91,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license!=="NONE"){
+    let typeOfLicense=`#### License ${renderLicenseBadge} \n ${renderLicenseLink}`;
+    return typeOfLicense;
+  }else{
+    ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -106,8 +113,7 @@ function generateMarkdown(data) {
   ${data.install} \
   ### Usage \
   ${data.instructions} \
-  #### License ${licenseLink} \
-  ${data.license} \
+  ${renderLicenseSection(data.license)} \
   renderLicenseLink(); \
   renderLicenseSection(); \
   ##### Contributing \
