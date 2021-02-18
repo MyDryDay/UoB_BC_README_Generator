53bae498f428985be9c6f53ed17a2e616289eb2b
George Cope <G.Cope13@gmail.com>
2021-02-18 06:45:54 +0000

    Tidied up the switch statement for the renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,45 +4,43 @@
 function renderLicenseBadge(data) {
 // [![GitHub license](https://img.shields.io/github/license/MyDryDay/${data.title}.svg)](https://github.com/MyDryDay/${data.title}/blob/main/LICENSE)`; 
 
-  constlicenseLink=``;
-  const gitLicense=data.license;
 
-  switch(gitLicense){
+  switch(license){
     case `Apache 2.0`:
-      licenseLink=`[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)(https://opensource.org/licenses/Apache-2.0)`;
       break;
     case `GNU General Public 3.0`:
-      licenseLink=`[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      return `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)(https://www.gnu.org/licenses/gpl-3.0)`;
       break;
     case `MIT`:
-      licenseLink=`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)`;
       break;
     case `BSD 2-Clause`:
-      licenseLink=`[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+      return `![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)(https://opensource.org/licenses/BSD-2-Clause)`;
       break;
     case `BSD 3-Clause`:
-      licenseLink=`[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+      return `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)(https://opensource.org/licenses/BSD-3-Clause)`;
       break;
     case `Creative Commons Zero v1.0`:
-      licenseLink=`[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+      return `![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)(http://creativecommons.org/publicdomain/zero/1.0/)`;
       break;
     case `Eclipse Public 2.0`:
-      licenseLink=`[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      return `![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)(https://opensource.org/licenses/EPL-1.0)`;
       break;
     case `GNU Affero General Public v3.0`:
-      licenseLink=`[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+      return `![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)(https://www.gnu.org/licenses/agpl-3.0)`;
       break;
     case `GNU General Public v2.0`:
       licenseLink=`[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
       break;
     case `Mozilla Public 2.0`:
-      licenseLink=`[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+      return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)(https://opensource.org/licenses/MPL-2.0)`;
       break;
     case `The Unlicense`:
-      licenseLink=`[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+      return `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)(http://unlicense.org/)`;
       break;
-    case `No License`:
-      licenseLink=``;
+    default:
+      return ``;
       break;
   }
 
